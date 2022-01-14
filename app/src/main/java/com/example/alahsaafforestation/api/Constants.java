268e2915e33ffcb0bfc9ac8d1c1598955072fbc6
Alaaa-Drebate <87737980+Alaaa-Drebate@users.noreply.github.com>
2022-01-14 02:45:52 +0200

    Update Constants.java

--- app/src/main/java/com/example/alahsaafforestation/api/Constants.java
+++ app/src/main/java/com/example/alahsaafforestation/api/Constants.java
@@ -11,25 +11,25 @@ public class Constants {
     public static final int VOLUNTEER_AVAILABLE = 1;
     public static final int VOLUNTEER_UNAVAILABLE = 0;
 
-    public static final String BASE_URL = "http://nawar.scit.co/trees-api/index.php";
-    public static final String LOGIN_URL = "http://nawar.scit.co/trees-api/index.php?type=login";
-    public static final String REGISTER_URL = "http://nawar.scit.co/trees-api/index.php?type=register";
-    public static final String UPDATE_USER_DATA = "http://nawar.scit.co/trees-api/index.php?type=update-user";
+    public static final String BASE_URL = "trees-api/index.php";
+    public static final String LOGIN_URL = "trees-api/index.php?type=login";
+    public static final String REGISTER_URL = "trees-api/index.php?type=register";
+    public static final String UPDATE_USER_DATA = "trees-api/index.php?type=update-user";
 
-    public static final String SERVICES_BY_VOLUNTEER = "http://nawar.scit.co/trees-api/index.php?type=get_services_by_volunteer_id";
+    public static final String SERVICES_BY_VOLUNTEER = "trees-api/index.php?type=get_services_by_volunteer_id";
 
-    public static final String PHARMACIES_ALL_URL = "http://nawar.scit.co/trees-api/index.php?type=get_acriculture_pharmacies";
+    public static final String PHARMACIES_ALL_URL = "trees-api/index.php?type=get_acriculture_pharmacies";
 
-    public static final String PRODUCTS_ALL_URL = "http://nawar.scit.co/trees-api/index.php?type=get_products";
-    public static final String PRODUCTS_BY_SELLER_ID = "http://nawar.scit.co/trees-api/index.php?type=get_products_by_seller_id";
+    public static final String PRODUCTS_ALL_URL = "trees-api/index.php?type=get_products";
+    public static final String PRODUCTS_BY_SELLER_ID = "trees-api/index.php?type=get_products_by_seller_id";
 
-    public static final String GET_MESSAGES_WITH = "http://nawar.scit.co/trees-api/index.php?type=get_messages_with";
+    public static final String GET_MESSAGES_WITH = "trees-api/index.php?type=get_messages_with";
 
-    public static final String GET_MESSAGES_LIST = "http://nawar.scit.co/trees-api/index.php?type=get-messages-list";
+    public static final String GET_MESSAGES_LIST = "trees-api/index.php?type=get-messages-list";
 
-    public static final String GET_ALL_VOLUNTEERS = "http://nawar.scit.co/trees-api/index.php?type=get_volunteers";
+    public static final String GET_ALL_VOLUNTEERS = "trees-api/index.php?type=get_volunteers";
 
-    public static final String RESET_PASSWORD = "http://nawar.scit.co/trees-api/index.php?type=reset_password";
+    public static final String RESET_PASSWORD = "index.php?type=reset_password";
     
 
 
